08237a8331a757277cbde91c8818d39ecc4c5cdc
Corentin Dominguez <corentin.dmz@live.fr>
2020-09-06 17:22:29 +0200

    amélioration de la navbar

--- src/composants/Home.js
+++ src/composants/Home.js
@@ -7,83 +7,6 @@ import './Home.css';
 class Home extends Component{
 
     render() {
-
-        /*
-        <CardDeck style={{marginTop: '15rem'}}>
-
-            <div style={{width: '50rem'}}>
-                <Card className={"bg-dark text-white"} style={{ width: '30rem' , marginLeft: '10rem', marginRight:'7rem'}}>
-                    <Card.Body>
-                        <Card.Header>
-                            <h3>Work Experience</h3>
-                        </Card.Header>
-                        <Card.Text>
-                            <h5 className={"mt-5"} align={"justify"}>Projet Declick 4'Teens</h5>
-                            <h6 className={"font-italic py-2"} align={"justify"}>Collège Lautrec, Toulouse</h6>
-                            <div align={"justify"}>
-                                Creation of a video game with middle school pupils on a platform created for it.
-                                We are three students for fifteen pupils.
-                                I learn, weeks after weeks, to lead a team of young people and among others to fix some conflicts.
-                            </div>
-
-                            <h5 className={"pt-5"} align={"justify"}>Develop a Python Game</h5>
-                            <h6 className={"font-italic py-2"} align={"justify"}>Personal project</h6>
-                            <div align={"justify"}>
-                                Creation of a platform game with two players in Python language.
-                                It allowed me to know how the “ object technology” works in Python.
-                                Moreover, it was a team project (6 persons),
-                                so I learned to work in a group and this during a few months.
-                            </div>
-
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-
-
-            <div className={"ml-5"}></div>
-
-            <div style={{width: "50rem"}}>
-
-                <Card className={"bg-dark text-white"} style={{marginLeft: '7rem', marginRight: '10rem'}}>
-                    <Card.Body>
-                        <Card.Header>
-                            <h3>Computer Skills</h3>
-                        </Card.Header>
-                        <Card.Text>
-                            <div align={"justify"}>
-                                <ul>
-                                    <li className={"my-4"}>Java +++</li>
-                                    <li className={"my-4"}>C +++</li>
-                                    <li className={"my-4"}>Ada ++</li>
-                                    <li className={"my-4"}>Python ++</li>
-                                </ul>
-                            </div>
-                        </Card.Text>
-                        <Card.Header>
-                            <h3>Personal Skills</h3>
-                        </Card.Header>
-                        <Card.Text>
-                            <div align={"justify"}>
-                                <ul>
-                                    <li className={"my-4"}>Good communication skills</li>
-                                    <li className={"my-4"}>Team spirit</li>
-                                    <li className={"my-4"}>Autonomous</li>
-                                    <li className={"mt-4"}>Flexible</li>
-                                </ul>
-                            </div>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-
-        </CardDeck>
-
-
-
-        <h5 className={"d-flex justify-content-left"} style={{marginLeft:'20rem'}}><u>ENSEEIHT, Toulouse (France)</u> -- <i>Digital sciences</i></h5>
-        */
-
         return(
             <div className={""}>
 
@@ -154,7 +77,6 @@ class Home extends Component{
                         </Card.Body>
                     </Card>
                     </div>
-
                 </CardDeck>
 
                 <Jumbotron className={"mt-5"} style={{display: "flex", justifyContent: "center", flexDirection: "column", paddingTop: "5px"}}>
--- src/composants/Menu.js
+++ src/composants/Menu.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {Navbar, Nav, Image, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
-//import './Menu.css';
+import './Menu.css';
 
 class Menu extends Component {
 
@@ -11,9 +11,8 @@ class Menu extends Component {
        <NavDropdown.Item href="/leadership">Leadership & Management</NavDropdown.Item>
     */
 
-    render() {
-
-        const appNavbar = (
+    /*
+            const appNavbar = (
             <Navbar bg={"dark"} expand={"lg"}>
                 <Navbar.Brand className={"d-inline p-2 text-white"}>
                     E-portfolio <br/>
@@ -50,6 +49,50 @@ class Menu extends Component {
                         </div>
                     </Navbar.Text>
                 </Navbar.Collapse>
+            </Navbar>
+        );
+     */
+
+
+    render() {
+
+        const appNavbar = (
+            <Navbar bg={"dark"} expand={"lg"}>
+                <div id={"brandNav"}>
+                <Navbar.Brand className={"d-inline p-2 text-white"}>
+                    E-portfolio <br/>
+                    Corentin Dominguez <br/>
+                </Navbar.Brand>
+                </div>
+                <Navbar.Toggle aria-controls={"justify-content-left"}/>
+                <Navbar.Collapse>
+                    <Nav style={{ width: "85%", display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+                        <NavLink className={"bg-dark text-white"} to={"/"}><h3>Home</h3></NavLink>
+                        <NavLink className={"bg-dark text-white"} to={"/spanish"}><h3>Spanish</h3></NavLink>
+                        <NavDropdown className={"pt-4 bg-dark text-white"} title={<h3 className={"text-white"}>Leadership & Management</h3>} id="basic-nav-dropdown">
+                            <NavDropdown.Item><Link to={"/leadership"}>Leadership & Management</Link></NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item><Link to={"/civicengagement"}>Civic Engagement</Link></NavDropdown.Item>
+                            <NavDropdown.Item>SWOT</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Text className={"d-inline p-2 bg-dark text-white"}>
+                    <h3 align={"justify"}>Contact :</h3>
+                    <div align={"justify"}>
+                        8 Place du Pont Neuf <br/>
+                        31000 Toulouse (France) <br/>
+                        +33 6 77 65 24 94 <br/>
+                        corentin.dmz@live.fr <br/>
+                        <div className={"mt-2"}></div>
+                        <a href="https://www.facebook.com/corentin.dmz.9" target={"_blank"}>
+                            <Image src={require("../icone_fb32.png")} width={"32px"} height={"32px"}/>
+                        </a>
+                        <a className={"pl-2"} href="https://github.com/Corentin648" target={"_blank"}>
+                            <Image src={require("../logo_github.png")} width={"32px"} height={"32px"}/>
+                        </a>
+                    </div>
+                </Navbar.Text>
             </Navbar>
         );
 
