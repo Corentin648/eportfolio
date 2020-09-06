7c46a17cc670c99cd7ae69ac8e9578837936eb6a
Corentin Dominguez <corentin.dmz@live.fr>
2020-09-06 18:02:22 +0200

    début amélioration page Spanish.js

--- src/composants/Spanish.js
+++ src/composants/Spanish.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
 import ReactPlayer from "react-player";
 
-
 class Spanish extends Component{
 
     render() {
         return(
             <div>
                 <h1 className={"mt-5"}><u>Spanish</u></h1>
-                <h3
-                    className={"mt-5 d-flex justify-content-left"}
-                    style={{marginLeft:'20rem'}}
-                > Project 1 -- Video presenting myself</h3>
-
-                <ReactPlayer
-                    className={"mt-5"}
-                    style={{marginLeft:'20rem'}}
-                    url={"https://youtu.be/dVFWuxUV7UE "}>
-                </ReactPlayer>
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px"}}>
+                    <p style={{border: "1px solid black"}}>Project 1 -- Video presenting myself</p>
+                    <ReactPlayer style={{border: "10px solid black"}} url={"https://youtu.be/dVFWuxUV7UE "} ></ReactPlayer>
+                </div>
             </div>
         )
     }
