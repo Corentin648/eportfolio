ae842a26133413dc86d2b6f47136ce2d28247961
Corentin Dominguez <corentin.dmz@live.fr>
2020-09-09 19:13:04 +0200

    amélioration video responsive Spanish et L&M

--- src/composants/LeadershipManagement.js
+++ src/composants/LeadershipManagement.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactPlayer from "react-player";
+import "./LeadershipManagement.css";
 
 
 class LeadershipManagement extends Component{
@@ -8,16 +9,10 @@ class LeadershipManagement extends Component{
         return(
             <div>
                 <h1 className={"mt-5"}><u>Leadership & Management</u></h1>
-                <h3
-                    className={"mt-5 d-flex justify-content-left"}
-                    style={{marginLeft:'20rem'}}
-                > Project 1 -- Video presenting my choice for my second year field at ENSEEIHT</h3>
-
-                <ReactPlayer
-                    className={"mt-5"}
-                    style={{marginLeft:'20rem'}}
-                    url={" https://youtu.be/K_phshsuixc"}>
-                </ReactPlayer>
+                <div style={{display: "flex", flexDirection: "column", marginTop: "50px", marginLeft:"10%"}}>
+                    <h3 style={{textAlign: "left"}}>Project 1 -- Video presenting my choice for my second year field at ENSEEIHT</h3>
+                    <div className={"player-wrapper"}><ReactPlayer height={"100%"} width={"100%"} className={"player"} url={" https://youtu.be/K_phshsuixc"} /></div>
+                </div>
             </div>
         )
     }
--- src/composants/Spanish.js
+++ src/composants/Spanish.js
@@ -10,7 +10,7 @@ class Spanish extends Component{
                 <h1 className={"mt-5"}><u>Spanish</u></h1>
                 <div style={{display: "flex", flexDirection: "column", marginTop: "50px", marginLeft:"10%"}}>
                     <h3 style={{textAlign: "left"}}>Project 1 -- Video presenting myself</h3>
-                    <div className={"player-wrapper"} style={{border: "1px solid black"}} ><ReactPlayer height={"100%"} width={"100%"} className={"player"} url={"https://youtu.be/dVFWuxUV7UE "} /></div>
+                    <div className={"player-wrapper"}><ReactPlayer height={"100%"} width={"100%"} className={"player"} url={"https://youtu.be/dVFWuxUV7UE "} /></div>
                 </div>
             </div>
         )
