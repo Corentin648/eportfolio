ae2b34c6338beeb80b33b0ab71521cb3009ab69e
Corentin Dominguez <corentin.dmz@live.fr>
2020-11-30 19:44:01 +0100

    ajout des ébauches de docs PPP

--- src/composants/PPP.js
+++ src/composants/PPP.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './PPP.css';
-import Pdf from '../Resume_Corentin_Dominguez.pdf';
+import resume from '../Resume_Corentin_Dominguez.pdf';
+import job_ad from '../job_ad_PPP.png';
+import ppt from '../presentation_PPP.pdf';
 
 class PPP extends Component{
 
@@ -9,11 +11,16 @@ class PPP extends Component{
 
             <div>
                 <h1 style={{marginTop: "50px", textAlign: "center"}}>Welcome to my PPP page</h1><br/>
-                <h3 style={{marginTop: "50px", textAlign: "left"}}>Here is the job ad I have chosen</h3>
-                <h3 id={"pdf_resume"} style={{width: "100%", marginTop: "50px", textAlign: "left"}}>
-                    <a href = {Pdf} target = "_blank">You can download my resume here</a>
+
+                <h3 className={"link"}>
+                    <a href = {job_ad} target = "_blank">Here is the job ad I have chosen</a>
                 </h3>
-                <h3 style={{marginTop: "50px", textAlign: "left"}}>Here is my presentation's PPT</h3><br/>
+                <h3 className={"link"}>
+                    <a href = {resume} target = "_blank">You can download my resume here</a>
+                </h3>
+                <h3 className={"link"}>
+                    <a href = {ppt} target = "_blank">Here is my presentation's PPT</a>
+                </h3>
 
             </div>
 
