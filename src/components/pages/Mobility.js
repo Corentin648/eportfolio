5162bea425d5eff76add7af3f08ee5e33440c636
Corentin648 <corentin.dmz@live.fr>
2022-09-18 00:59:15 +0200

    début rédaction mobitlité (puis passage sur drive)

--- src/components/pages/Mobility.js
+++ src/components/pages/Mobility.js
@@ -6,7 +6,16 @@ class Mobility extends Component{
     render() {
         return(
             <div>
-                Hello World
+                Je présente ici mon projet de mobilité internationale dans le cadre de mes études à l'N7.
+                J'ai eu la chance de réaliser durant une IVE, un tour d'Europe en voiture, voyage durant lequel j'ai à
+                la fois travaillé à l'aide de la plateforme Workaway mais aussi visité de nombreux pays !
+                Ainsi, je vais pouvoir ici parler plus en détail des expériences de Workaway que j'ai pu avoir. Si vous
+                voulez consulter la partie roadtrip de mon voyage, vous pouvez aller jeter un oeil à mon Polarsteps.
+
+
+                Workaway, qu'est ce c'est ?
+                C'est une plateforme web mondiale qui met en relations des "hosts" et des "travelers" (moi). Les hosts
+                proposent un travail, souvent agricole, non rémunéré mais en l'échange d'un lit de nouriture.
             </div>
         )
     }
