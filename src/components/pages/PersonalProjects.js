d316799150174d960271f8c001d0db0305720b4f
Corentin648 <corentin.dmz@live.fr>
2022-10-02 18:59:01 +0200

    amélioration design card

--- src/components/pages/PersonalProjects.js
+++ src/components/pages/PersonalProjects.js
@@ -12,23 +12,23 @@ class PersonalProjects extends Component{
     render() {
         return(
 
-            <section className={"flex flex-col w-full justify-center"}>
-                <div className={"w-3/5 mt-8 flex justify-between items-center"}>
+            <section className={"flex w-full justify-center"}>
+{/*                <div className={"w-3/5 mt-8 flex justify-between items-center"}>
                     <div className={"flex flex-col items-left"}>
                         <h1 className={`${Theme.textPrimaryColor} text-3xl md:text-5xl md:leading-normal text-left`}>
                             You can look at my github here
                         </h1>
                     </div>
-                </div>
-                <div className={"w-full lg:w-[500px] mb-[30px] lg:mb-0 lg:mr-[50px] flex flex-col items-center"}>
-                    <div className={`w-full flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
-                        <div className={"w-4/5 p-4"}>
+                </div>*/}
+                <div className={"w-full lg:w-4/5 flex flex-col md:flex-row justify-center items-center"}>
+                    <div className={`w-full lg:w-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                        <div className={"w-full rounded-t-md"}>
                             <img
-                            className={""}
+                            className={"rounded-t-md"}
                             src={home_site_campus}
                             alt={""}/>
                         </div>
-                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Camp US website</h3>
+                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Camp US website - 2021</h3>
                         <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, Sass</div>
                         <span className={"w-full p-4 text-lg text-justify"}>
                             A front-end website presenting my father's association, organising each year a military vehicles show
@@ -48,15 +48,15 @@ class PersonalProjects extends Component{
                             </Link>
                         </div>
                     </div>
-                    <div className={`w-full flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
-                        <div className={"w-4/5 p-4"}>
+                    <div className={`w-full lg:w-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                        <div className={"w-full rounded-t-md"}>
                             <img
-                                className={""}
+                                className={"rounded-t-md"}
                                 src={home_eportfolio}
                                 alt={""}/>
                         </div>
-                        <h3 className={"w-full p-4 text-left text-2xl font-medium"}>Eportfolio</h3>
-                        <div className={"w-full p-4 text-md text-[#008b8b] text-left italic"}>React.JS, Tailwind CSS</div>
+                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Eportfolio - 2020</h3>
+                        <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, Tailwind CSS</div>
                         <span className={"w-full p-4 text-lg text-justify"}>
                             The website you're currently browsing, presenting me and my projects
                         </span>
@@ -73,35 +73,6 @@ class PersonalProjects extends Component{
                                 to={"/personalprojects"}>
                                 <FontAwesomeIcon icon={faGithub}/>
                             </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className={"w-full lg:w-4/5 h-max mt-[50px] mb-[30px] lg:mb-0 " + Theme.cardClass}>
-                    <div className={Theme.cardHeaderClass}>
-                        My projects
-                        <a href="https://github.com/Corentin648"
-                           target={"_blank"}
-                           rel={"noopener noreferrer"}>
-                            <FontAwesomeIcon className={`ml-2 ${Theme.textPrimaryColor}`} icon={faGithub}/>
-                        </a>
-                    </div>
-
-                    <div className={"text-white text-justify px-3 pb-8"}>
-                        <h5 className={"mt-5"} align={"justify"}>Game of life Web <a href="src/components/pages/PersonalProjects" style={{color: "white"}} target={"_blank"} rel={"noopener noreferrer"}>(here)</a>
-                        </h5>
-                        <h6 className={"font-italic py-2"} align={"justify"}>September 2020</h6>
-                        <div align={"justify"}>
-                            Creation of a web site with Reat.js. It aims to parameter a game of life. We can choose size of the grid,
-                            size of the cases but also rules of life and death that are applied in game.
-                            We can also act on the grid like add a pattern, play and pause the game and make it progress step by step or step back.
-                        </div>
-                        <h5 className={"pt-5"} align={"justify"}>Develop a Python Game</h5>
-                        <h6 className={"font-italic py-2"} align={"justify"}>March - May 2018</h6>
-                        <div align={"justify"}>
-                            Creation of a platform game with two players in Python language.
-                            It allowed me to know how the “ object technology” works in Python.
-                            Moreover, it was a team project (6 persons),
-                            so I learned to work in a group and this during a few months.
                         </div>
                     </div>
                 </div>
