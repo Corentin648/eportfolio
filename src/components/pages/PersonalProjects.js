8947e8f6d8df752980b4b6de1c6972a06329d39e
Corentin648 <corentin.dmz@live.fr>
2022-10-02 19:19:03 +0200

    design cards grands écrans OK

--- src/components/pages/PersonalProjects.js
+++ src/components/pages/PersonalProjects.js
@@ -12,16 +12,12 @@ class PersonalProjects extends Component{
     render() {
         return(
 
-            <section className={"flex w-full justify-center"}>
-{/*                <div className={"w-3/5 mt-8 flex justify-between items-center"}>
-                    <div className={"flex flex-col items-left"}>
-                        <h1 className={`${Theme.textPrimaryColor} text-3xl md:text-5xl md:leading-normal text-left`}>
-                            You can look at my github here
-                        </h1>
-                    </div>
-                </div>*/}
-                <div className={"w-full lg:w-4/5 flex flex-col md:flex-row justify-center items-center"}>
-                    <div className={`w-full lg:w-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+            <section className={"w-full flex flex-col items-center"}>
+                <h1 className={`w-full mt-[50px] text-3xl text-center`}>
+                    My projects
+                </h1>
+                <div className={"w-full lg:w-4/5 my-[80px] flex flex-col md:flex-row justify-center items-center"}>
+                    <div className={`relative w-full lg:w-[400px] lg:h-[450px] lg:mr-[50px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
                         <div className={"w-full rounded-t-md"}>
                             <img
                             className={"rounded-t-md"}
@@ -33,22 +29,22 @@ class PersonalProjects extends Component{
                         <span className={"w-full p-4 text-lg text-justify"}>
                             A front-end website presenting my father's association, organising each year a military vehicles show
                         </span>
-                        <div className={"w-full mt-4 flex"}>
+                        <div className={"absolute bottom-0 w-full mt-4 flex"}>
                             <a
-                                  className={"bg-[#444] border-r border-[#333] rounded-bl-md p-3 w-1/2 text-center text-2xl"}
+                              className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
                                   href={"https://camp-us-casson44.fr/#/"}
                                   target={"_blank"} rel={"noopener noreferrer"}
                             >
                                 Try it
                             </a>
                             <Link
-                                  className={"bg-[#444] border-l border-[#333] rounded-br-md p-3 w-1/2 text-center text-3xl"}
+                                  className={"bg-[#444] border-l border-[#333] rounded-br-md p-1 w-1/2 text-center text-3xl"}
                                   to={"/personalprojects"}>
                                 <FontAwesomeIcon icon={faGithub}/>
                             </Link>
                         </div>
                     </div>
-                    <div className={`w-full lg:w-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                    <div className={`relative w-full lg:w-[400px] lg:h-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
                         <div className={"w-full rounded-t-md"}>
                             <img
                                 className={"rounded-t-md"}
@@ -60,19 +56,18 @@ class PersonalProjects extends Component{
                         <span className={"w-full p-4 text-lg text-justify"}>
                             The website you're currently browsing, presenting me and my projects
                         </span>
-                        <div className={"w-full mt-4 flex"}>
-                            <a
-                                className={"bg-[#444] border-r border-[#333] rounded-bl-md p-3 w-1/2 text-center text-2xl"}
-                                href={"https://corentin648.github.io/"}
-                                target={"_blank"} rel={"noopener noreferrer"}
-                            >
-                                Try it
-                            </a>
+                        <div className={"absolute bottom-0 w-full mt-4 flex"}>
                             <Link
-                                className={"bg-[#444] border-l border-[#333] rounded-br-md p-3 w-1/2 text-center text-3xl"}
-                                to={"/personalprojects"}>
-                                <FontAwesomeIcon icon={faGithub}/>
+                                className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
+                                to={"/"}>
+                                Try it
                             </Link>
+                            <a
+                                className={"bg-[#444] border-l border-[#333] rounded-br-md p-1 w-1/2 text-center text-3xl"}
+                                href={"https://github.com/Corentin648/eportfolio"}
+                                target={"_blank"} rel={"noopener noreferrer"}>
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
                         </div>
                     </div>
                 </div>
