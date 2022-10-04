46aa1060d55b8e3ebc520e1669a54f9aee68c5d3
Corentin648 <corentin.dmz@live.fr>
2022-10-04 17:14:40 +0200

    ajout du projet jeu de la vie

--- src/components/pages/PersonalProjects.js
+++ src/components/pages/PersonalProjects.js
@@ -6,6 +6,7 @@ import {faGithub} from "@fortawesome/free-brands-svg-icons";
 import {Link} from "react-router-dom";
 import home_site_campus from "../../assets/home_site_camp_us.png";
 import home_eportfolio from "../../assets/home_eportfolio.png";
+import home_jeu_de_la_vie from "../../assets/home_jeu_de_la_vie.png"
 
 class PersonalProjects extends Component{
 
@@ -65,6 +66,33 @@ class PersonalProjects extends Component{
                             <a
                                 className={"bg-[#444] border-l border-[#333] rounded-br-md p-1 w-1/2 text-center text-3xl"}
                                 href={"https://github.com/Corentin648/eportfolio"}
+                                target={"_blank"} rel={"noopener noreferrer"}>
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className={`lg:relative w-full md:w-3/5 lg:w-[400px] lg:h-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                        <div className={"w-full rounded-t-md"}>
+                            <img
+                                className={"rounded-t-md"}
+                                src={home_jeu_de_la_vie}
+                                alt={""}/>
+                        </div>
+                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Jeu de la vie - 2020</h3>
+                        <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, JS</div>
+                        <span className={"w-full p-4 text-lg text-justify"}>
+                            A simple web project to monitor a Game of Life
+                        </span>
+                        <div className={"lg:absolute lg:bottom-0 w-full mt-4 flex"}>
+                            <a
+                                className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
+                                href={"https://github.com/Corentin648"}
+                                target={"_blank"} rel={"noopener noreferrer"}>
+                                Try it
+                            </a>
+                            <a
+                                className={"bg-[#444] border-l border-[#333] rounded-br-md p-1 w-1/2 text-center text-3xl"}
+                                href={"https://github.com/Corentin648/GameOfLifeWeb"}
                                 target={"_blank"} rel={"noopener noreferrer"}>
                                 <FontAwesomeIcon icon={faGithub}/>
                             </a>
