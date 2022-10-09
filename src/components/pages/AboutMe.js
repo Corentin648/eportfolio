e27e01f7ecdb1a64dd66ada01e7d2256523f64c4
Corentin648 <corentin.dmz@live.fr>
2022-10-09 19:54:05 +0200

    homogenisation affichage page about

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -18,8 +18,8 @@ class AboutMe extends Component{
     render() {
         return(
             <div id={"about-section"} className={"w-full mt-24 flex flex-col items-center"}>
-                <div className={"w-3/5"}>
-                    <h1 className={"text-2xl underline underline-offset-4"}>About me</h1>
+                <div className={"w-3/5 mb-32"}>
+                    <h1 className={"mb-8 text-2xl underline underline-offset-4"}>About me</h1>
                     <div className={"flex flex-col lg:flex-row"}>
                        <div className={"w-2/5"}>
                            <img
@@ -51,53 +51,42 @@ class AboutMe extends Component{
                         </div>
                     </div>
                 </div>
-
-
-
-                <div className={"w-4/5 lg:w-4/5 flex flex-col lg:flex-row lg:justify-center"}>
-                    <div className={"w-full lg:w-[500px] mb-[30px] lg:mb-0 lg:mr-[50px] flex flex-col items-center"}>
-                        <div className={Theme.cardHeaderClass}>
-                            Some of my programming skills
-                            <a href="https://github.com/Corentin648"
-                               target={"_blank"}
-                               rel={"noopener noreferrer"}>
-                                <FontAwesomeIcon className={`ml-2 ${Theme.textPrimaryColor}`} icon={faGithub}/>
-                            </a>
+                <div className={"w-3/5 mb-32 flex flex-col items-center"}>
+                    <h1 className={"w-full mb-8 text-2xl text-left underline underline-offset-4"}>
+                        Some of my programming skills
+                    </h1>
+                    <div className={`w-full lg:w-[500px] flex flex-col rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                        <div className={"my-2"}>
+                            <h5 className={"p-2"}>Frontend</h5>
+                            <div className={"p-2 flex justify-center"}>
+                                <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faReact} />React.js</div>
+                                <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faAngular} />Angular</div>
+                                <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faCss3} />TailwindCSS</div>
+                                <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faSass} />Sass</div>
+                            </div>
                         </div>
-
-                        <div className={`w-full flex flex-col rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
-                            <div className={"my-2"}>
-                                <h5 className={"p-2"}>Frontend</h5>
-                                <div className={"p-2 flex justify-center"}>
-                                    <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faReact} />React.js</div>
-                                    <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faAngular} />Angular</div>
-                                    <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faCss3} />TailwindCSS</div>
-                                    <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faSass} />Sass</div>
-                                </div>
+                        <div className={"my-2"}>
+                            <h5 className={"p-2"}>Backend</h5>
+                            <div className={"p-2 flex justify-center"}>
+                                <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faPhoenixFramework} />Elixir (Phoenix)</div>
+                                <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faDatabase} />PostgreSQL</div>
                             </div>
-                            <div className={"my-2"}>
-                                <h5 className={"p-2"}>Backend</h5>
-                                <div className={"p-2 flex justify-center"}>
-                                    <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faPhoenixFramework} />Elixir (Phoenix)</div>
-                                    <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faDatabase} />PostgreSQL</div>
-                                </div>
+                        </div>
+                        <div className={"my-2"}>
+                            <h5 className={"p-2"}>Other programming languages</h5>
+                            <div className={"p-2 flex"}>
+                                <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faJava} />Java</div>
+                                <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faC} /></div>
+                                <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faPython} />Python</div>
+                                <div className={"py-1 px-2 border rounded-r-xl"}>Ada</div>
                             </div>
-                            <div className={"my-2"}>
-                                <h5 className={"p-2"}>Other programming languages</h5>
-                                <div className={"p-2 flex"}>
-                                    <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faJava} />Java</div>
-                                    <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faC} /></div>
-                                    <div className={"py-1 px-2 border"}><FontAwesomeIcon className={"pr-2"} icon={faPython} />Python</div>
-                                    <div className={"py-1 px-2 border rounded-r-xl"}>Ada</div>
-                                </div>
-                            </div>
-                            <div className={"my-2"}>
-                                <h5 className={"p-2"}>Tools</h5>
-                                <div className={"p-2 flex"}>
-                                    <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faGithub} />Github</div>
-                                    <div className={"py-1 px-2 border"}>Semaphore CI</div>
-                                    <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faDocker} />Docker</div>
-                                </div>
+                        </div>
+                        <div className={"my-2"}>
+                            <h5 className={"p-2"}>Tools</h5>
+                            <div className={"p-2 flex"}>
+                                <div className={"py-1 px-2 border rounded-l-xl"}><FontAwesomeIcon className={"pr-2"} icon={faGithub} />Github</div>
+                                <div className={"py-1 px-2 border"}>Semaphore CI</div>
+                                <div className={"py-1 px-2 border rounded-r-xl"}><FontAwesomeIcon className={"pr-2"} icon={faDocker} />Docker</div>
                             </div>
                         </div>
                     </div>
