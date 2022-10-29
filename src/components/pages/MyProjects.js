8fe9350ec3832a53862b7ba62632524c371d5435
Corentin648 <corentin.dmz@live.fr>
2022-10-29 16:48:26 +0200

    clean code MyProjects.js

--- src/components/pages/MyProjects.js
+++ src/components/pages/MyProjects.js
@@ -10,6 +10,12 @@ import home_jeu_de_la_vie from "../../assets/home_jeu_de_la_vie.webp"
 
 class MyProjects extends Component{
 
+    static cardClass = `lg:relative w-full sm:w-4/5 md:w-3/5 h-max lg:w-[400px] lg:h-[450px] lg:mb-0 flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-md shadow-black/50`;
+    static cardTitleClass = "w-full p-3 text-left text-2xl font-medium";
+    static cardTagsClass = `w-full pl-4 pb-2 text-md ${Theme.textSecondaryColor} text-left italic`;
+    static cardTextClass = "w-full p-4 text-lg text-justify";
+    static cardFooterClass = "lg:absolute lg:bottom-0 w-full mt-4 flex";
+
     render() {
         return(
 
@@ -17,20 +23,20 @@ class MyProjects extends Component{
                 <h1 className={`w-full mt-[50px] text-3xl text-center`}>
                     My projects
                 </h1>
-                <div className={"w-4/5 my-[80px] grid lg:grid-cols-2 2xl:grid-cols-3 gap-8 place-items-center"}>
-                    <div className={`lg:relative w-full sm:w-4/5 md:w-3/5 h-max lg:w-[400px] lg:h-[450px] lg:mb-0 flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                <div id={"cards-grid"} className={"w-4/5 my-[80px] grid lg:grid-cols-2 2xl:grid-cols-3 gap-8 place-items-center"}>
+                    <div className={MyProjects.cardClass}>
                         <div className={"w-full rounded-t-md"}>
                             <img
                             className={"rounded-t-md"}
                             src={home_site_campus}
                             alt={""}/>
                         </div>
-                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Camp US website - 2021</h3>
-                        <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, Sass</div>
-                        <span className={"w-full p-4 text-lg text-justify"}>
+                        <h3 className={MyProjects.cardTitleClass}>Camp US website - 2021</h3>
+                        <div className={MyProjects.cardTagsClass}>React.JS, Sass</div>
+                        <span className={MyProjects.cardTextClass}>
                             A front-end website presenting my father's association, organising each year a military vehicles show
                         </span>
-                        <div className={"lg:absolute lg:bottom-0 w-full mt-4 flex"}>
+                        <div className={MyProjects.cardFooterClass}>
                             <a
                               className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
                                   href={"https://camp-us-casson44.fr/#/"}
@@ -45,19 +51,19 @@ class MyProjects extends Component{
                             </Link>
                         </div>
                     </div>
-                    <div className={`lg:relative w-full sm:w-4/5 md:w-3/5 lg:w-[400px] lg:h-[450px] flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                    <div className={MyProjects.cardClass}>
                         <div className={"w-full rounded-t-md"}>
                             <img
                                 className={"rounded-t-md"}
                                 src={home_eportfolio}
                                 alt={""}/>
                         </div>
-                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Eportfolio - 2020</h3>
-                        <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, Tailwind CSS</div>
-                        <span className={"w-full p-4 text-lg text-justify"}>
+                        <h3 className={MyProjects.cardTitleClass}>Eportfolio - 2020</h3>
+                        <div className={MyProjects.cardTagsClass}>React.JS, Tailwind CSS</div>
+                        <span className={MyProjects.cardTextClass}>
                             The website you're currently browsing, presenting me and my projects
                         </span>
-                        <div className={"lg:absolute lg:bottom-0 w-full mt-4 flex"}>
+                        <div className={MyProjects.cardFooterClass}>
                             <Link
                                 className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
                                 to={"/"}>
@@ -71,19 +77,19 @@ class MyProjects extends Component{
                             </a>
                         </div>
                     </div>
-                    <div className={`lg:relative w-full sm:w-4/5 md:w-3/5 lg:w-[400px] lg:h-[450px] lg:col-span-2 2xl:col-span-1 flex flex-col items-center rounded-md text-white ${Theme.bgPrimaryColor} shadow-2xl shadow-black`}>
+                    <div className={MyProjects.cardClass + " lg:col-span-2 2xl:col-span-1"}>
                         <div className={"w-full rounded-t-md"}>
                             <img
                                 className={"rounded-t-md"}
                                 src={home_jeu_de_la_vie}
                                 alt={""}/>
                         </div>
-                        <h3 className={"w-full p-3 text-left text-2xl font-medium"}>Jeu de la vie - 2020</h3>
-                        <div className={"w-full pl-4 pb-2 text-md text-[#008b8b] text-left italic"}>React.JS, JS</div>
-                        <span className={"w-full p-4 text-lg text-justify"}>
+                        <h3 className={MyProjects.cardTitleClass}>Jeu de la vie - 2020</h3>
+                        <div className={MyProjects.cardTagsClass}>React.JS, JS</div>
+                        <span className={MyProjects.cardTextClass}>
                             A simple web project to monitor a Game of Life
                         </span>
-                        <div className={"lg:absolute lg:bottom-0 w-full mt-4 flex"}>
+                        <div className={MyProjects.cardFooterClass}>
                             <a
                                 className={"bg-[#444] border-r border-[#333] rounded-bl-md p-1 w-1/2 text-center text-2xl"}
                                 href={"https://github.com/Corentin648"}
