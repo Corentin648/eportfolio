82d11657263ed9946736b7fe6044d2d66829f42d
Corentin648 <corentin.dmz@live.fr>
2023-04-15 13:52:17 +0200

    fix(bundler): try fix mime text/html error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
+import * as path from "path";
+
+
+const useCredentialsPlugin = () => {
+    return {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+            return html.replace(
+                'crossorigin',
+                '',
+            );
+        },
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react({
         jsxRuntime: 'classic',
-    }), svgrPlugin()],
+    }), svgrPlugin(), useCredentialsPlugin()],
     build: {
         outDir: 'build',
     },
